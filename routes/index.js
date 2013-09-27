844822a439398cacd1134114aa5df456af562968
khurram <khurram@debian.debian>
2013-09-27 00:46:14 -0400

    added responsive template

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var data = {
+	title : "Researcher's Diary",
+	templateUrl : "https://github.com/verekia/initializr-template"
+  }
+  
+  res.render('index', data);
 };
